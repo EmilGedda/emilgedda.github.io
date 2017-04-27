bf36e5db429ce13d9baf8157499a9d715dae83a3
Emil Gedda <emil.gedda@emilgedda.se>
2017-04-27 21:24:37 +0200

    pdf generation for every post

--- site.hs
+++ site.hs
@@ -3,9 +3,15 @@
 import           Data.Monoid (mappend)
 import           Hakyll
 
-import Data.List.Split (splitOn)
-import Data.List (intercalate, isInfixOf)
-import System.FilePath.Posix (splitFileName, takeBaseName)
+
+import           System.FilePath (replaceExtension, takeDirectory)
+import           Data.List.Split (splitOn)
+import           Data.List (intercalate, isInfixOf)
+import           System.FilePath.Posix (splitFileName, takeBaseName, 
+                                        splitDirectories, takeDirectory)
+import           System.Process  (system)
+import qualified Text.Pandoc     as Pandoc
+
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
@@ -24,18 +30,27 @@ main = hakyll $ do
             >>= relativizeUrls
 
     match "posts/*" $ do
-        route   dateRoute 
+        route   dateRoute
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
             >>= removeIndexHtml
 
+    --Shamefully stolen from jaspervdj
+    match "posts/*" $ version "pdf" $ do
+        route   pdfRoute
+        compile $ do getResourceBody
+            >>= readPandoc
+            >>= (return . fmap writeXeTex)
+            >>= loadAndApplyTemplate "templates/post.tex" defaultContext
+            >>= xelatex
+
     create ["archive.html"] $ do
         route   folderRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAll ("posts/*" .&&. hasNoVersion)
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Archives"            `mappend`
@@ -47,13 +62,13 @@ main = hakyll $ do
                 >>= relativizeUrls
                 >>= removeIndexHtml
 
-    create ["atom.xml"] $ feeder 10 renderAtom 
+    create ["atom.xml"] $ feeder 10 renderAtom
     create ["rss.xml"]  $ feeder 10 renderRss
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAll ("posts/*" .&&. hasNoVersion)
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Home"                `mappend`
@@ -68,12 +83,31 @@ main = hakyll $ do
     match "templates/*" $ compile templateBodyCompiler
 
 
-feeder count renderer = do 
+
+writeXeTex = Pandoc.writeLaTeX Pandoc.def {Pandoc.writerTeXLigatures = False}
+
+xelatex :: Item String -> Compiler (Item TmpFile)
+xelatex item = do
+    TmpFile texPath <- newTmpFile "xelatex.tex"
+    let tmpDir  = takeDirectory texPath
+        pdfPath = replaceExtension texPath "pdf"
+
+    unsafeCompiler $ do
+        writeFile texPath $ itemBody item
+        _ <- system $ unwords ["xelatex", "-halt-on-error",
+            "-output-directory", tmpDir, texPath, ">/dev/null", "2>&1"]
+        _ <- system $ unwords ["xelatex", "-halt-on-error",
+            "-output-directory", tmpDir, texPath, ">/dev/null", "2>&1"]
+        return ()
+
+    makeItem $ TmpFile pdfPath
+
+feeder count renderer = do
     route idRoute
     compile $ do
         let feedCtx = postCtx `mappend` bodyField "description"
         posts <- fmap (take count) . recentFirst =<<
-            loadAllSnapshots "posts/*" "content"
+            loadAllSnapshots ("posts/*" .&&. hasNoVersion) "content"
         renderer myFeedConfiguration feedCtx posts
 
 myFeedConfiguration :: FeedConfiguration
@@ -88,10 +122,13 @@ myFeedConfiguration = FeedConfiguration
 folderRoute = customRoute $ (++"/index.html") . takeBaseName . toFilePath
 
 dateRoute = customRoute $ j . splitOn "-" . toFilePath
-    where j x = (intercalate "/"$ take 3 x) 
+    where j x = (intercalate "/"$ take 3 x)
                 ++ '/':(takeWhile (/='.'). intercalate "-" $ drop 3 x)
                 ++ "/index.html"
 
+pdfRoute = dateRoute `composeRoutes` customRoute (f . takeDirectory . toFilePath)
+    where g = last . splitDirectories 
+          f x = (takeDirectory x) ++ "/" ++ g x ++ ".pdf"
 removeIndexHtml :: Item String -> Compiler (Item String)
 removeIndexHtml item = return $ fmap (withUrls removeIndexStr) item
   where
